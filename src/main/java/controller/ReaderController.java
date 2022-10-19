33ba9fe2a2a6888ee41e93de5aa6fbd983245f85
Giang <doantragiang.nguyen@my.jcu.edu.au>
2022-10-19 13:54:23 +0700

    Add POST to add new reader

--- src/main/java/controller/ReaderController.java
+++ src/main/java/controller/ReaderController.java
@@ -1,5 +1,22 @@
 package controller;
 
-public class ReaderController {
+import entity.Reader;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.*;
+
+@RestController
+@RequestMapping("/readers")
+public class ReaderController {
+    private List<Reader> readers = new ArrayList<>();
+
+    @PostMapping("/addReader")
+    public ResponseEntity addReader(@RequestBody Reader reader){
+        readers.add(reader);
+        ResponseEntity.ok().body(reader);
+    }
 }
